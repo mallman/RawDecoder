f5d46a035dc9a90ecab60a287dac9b3211376bff
Michael Allman <msa@allman.ms>
2023-08-13 14:06:41 -0700

    Initial Commit

---
+++ RawDecoder/RawDecoder.h
@@ -0,0 +1,18 @@
+//
+//  RawDecoder.h
+//  RawDecoder
+//
+//  Created by Michael Allman on 8/13/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RawDecoder.
+FOUNDATION_EXPORT double RawDecoderVersionNumber;
+
+//! Project version string for RawDecoder.
+FOUNDATION_EXPORT const unsigned char RawDecoderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RawDecoder/PublicHeader.h>
+
+
